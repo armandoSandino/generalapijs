f69b5412909b83a0fe2afe932f490dd25394acc4
Arturo Vasquez <artav17@gmail.com>
2017-10-18 00:30:23 -0400

    cambios assets/general.js UPLOAD function

--- socket.php
+++ socket.php
@@ -2,6 +2,6 @@
 	session_start();
 	$JSON       = file_get_contents("php://input");
 	$request    = json_decode($JSON);
-	echo($request->variabu."\n");
-	echo($request->variabd);
+	echo($request->varu."\n");
+	echo($request->vard);
 ?>
